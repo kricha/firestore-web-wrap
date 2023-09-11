977073d1bdc355bf6746693ae539225375e3bcbe
Oleksii <4619899+kricha@users.noreply.github.com>
2023-09-11 17:47:47 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { App } from '@tinyhttp/app'
+import { logger } from '@tinyhttp/logger'
+import bodyParser from 'body-parser';
+import creds from '../firestore.json' assert { type: 'json' };
+
+import { cert, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import moment from 'moment-timezone';
+
+const app = new App();
+initializeApp({ credential: cert(creds) });
+
+const firestore = getFirestore();
+
+app
+    .use(logger())
+    .use(bodyParser.json())
+    .post('/trackAction', (req, res) => {
+        const { username, ip, data, type } = req.body;
+        const now = moment().tz('Europe/Kyiv');
+
+
+        firestore.doc(`login/${username}`).set({ [`action_${now.format('MM-DD-YY')}`]: { [`action_${now.format('HH-mm-ss')}`]: [ip, type, data] } }, { merge: true }).then(()=>{
+            res.json({ok: true});
+        })
+    })
+    .post('/trackSerf', (req, res) => {
+        const { username, ip, method, url } = req.body;
+        const now = moment().tz('Europe/Kyiv');
+        firestore.doc(`login/${username}`).set({ [`action_${now.format('MM-DD-YY')}`]: { [`action_${now.format('HH-mm-ss')}`]: [ip, method, url] } }, { merge: true }).then(()=>{
+            res.json({ok: true});
+        })
+    })
+    .post('/trackLogin', (req, res) => {
+        const { username, ip, method, url } = req.body;
+        const now = moment().tz('Europe/Kyiv');
+        firestore.doc(`login/${username}`).set({ [`${now.format('MM-DD-YY HH-mm-ss')}`]: ip }, { merge: true }).then(()=>{
+            res.json({ok: true});
+        })
+    })
+    
+    .listen(5555)
