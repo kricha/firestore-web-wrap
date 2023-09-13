e0165df653249ebf6b61ca04c4feb5ab062529af
Oleksii <4619899+kricha@users.noreply.github.com>
2023-09-13 20:55:10 +0300

    fix: change storing format

--- src/index.js
+++ src/index.js
@@ -18,23 +18,24 @@ app
     .post('/trackAction', (req, res) => {
         const { username, ip, data, type } = req.body;
         const now = moment().tz('Europe/Kyiv');
-        firestore.doc(`login/${username}`).set({ [`action_${now.format('MM-DD-YY')}`]: { [`${now.format('HH:mm:ss')}`]: [ip, type, data] } }, { merge: true }).then(()=>{
-            res.json({ok: true});
+        firestore.doc(`login/${username}`).set({ [`action_${now.format('MM-DD-YY')}`]: { [`${now.format('HH:mm:ss')} ${ip}`]: `${type} ${data}` } }, { merge: true }).then(() => {
+            res.json({ ok: true });
         })
     })
     .post('/trackSerf', (req, res) => {
         const { username, ip, method, url } = req.body;
         const now = moment().tz('Europe/Kyiv');
-        firestore.doc(`login/${username}`).set({ [`serf_${now.format('MM-DD-YY')}`]: { [`${now.format('HH:mm:ss')}`]: [ip, method, url] } }, { merge: true }).then(()=>{
-            res.json({ok: true});
+        firestore.doc(`login/${username}`).set({ [`serf_${now.format('MM-DD-YY')}`]: { [`${now.format('HH:mm:ss')} ${ip}`]: `Go to ${url}` } }, { merge: true }).then(() => {
+            res.json({ ok: true });
         })
     })
     .post('/trackLogin', (req, res) => {
         const { username, ip } = req.body;
         const now = moment().tz('Europe/Kyiv');
-        firestore.doc(`login/${username}`).set({ [`${now.format('MM-DD-YY HH:mm:ss')}`]: ip }, { merge: true }).then(()=>{
-            res.json({ok: true});
+        // firestore.doc(`login/${username}`).set({ [`${now.format('MM-DD-YY HH:mm:ss')}`]: ip }, { merge: true }).then(()=>{
+        firestore.doc(`login/${username}`).set({ [`${now.format('MM-DD-YY')}`]: { [`${now.format('HH:mm:ss')} ${ip}`]: 'LOGIN' } }, { merge: true }).then(() => {
+            res.json({ ok: true });
         })
     })
-    
+
     .listen(5555)
